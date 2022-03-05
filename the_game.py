fd800d5922e65b86a5451fbc375a45681eadab27
Eduard Alexandru <eduard.github@gmail.com>
2022-03-05 02:24:59 +0000

    intial commit

---
+++ the_game.py
@@ -0,0 +1,18 @@
+
+ 
+print("hello")
+
+print("hello")
+print("olleh")
+print("hello")
+
+player = {
+    "name": "enter name",
+    'life': 10,
+    'weapon': "none"
+}
+enemy = {
+    "goblin",
+    "troll",
+    "dragon",
+}
